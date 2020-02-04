afdf2502bf895b8ec0186a25729c37fa90bc7d07
John Wilson <john@noreply.com>
2020-02-03 20:00:52 -0500

    learning about YAML

---
+++ readyaml03.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# YAML is NOT part of the standard library
+# python3 -m pip install pyyaml
+import yaml
+
+def main():
+    ## Open a blob of YAML data
+    yammyfile = open("/home/student/pyapi/myYAML.yml", "r")
+
+    ## convert YAML into Python data structures (lists and dictionaries)
+    pyyammy = yaml.load(yammyfile)
+
+    # display our new Python data
+    print(pyyammy)
+
+if __name__ == "__main__":
+    main()
